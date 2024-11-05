73fe9ebcecfbc3e07b4aeed1be709b1ba4c1ed91
annah11 <hanamesfin67@gmail.com>
2024-11-05 15:50:23 +0300

    test

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const port = 3000;
 
 
 app.get("/", (req, res) => {
+    const today = new Date();
+    const day = today.getDay();
+    console.log(day)
     res.render('index.ejs', {
         dataType:"a weekday",advice:"Its's a day to work hard "});
     });
