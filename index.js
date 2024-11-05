cddaf04f1ac7435a3e138096efc6338f9d8d45d7
annah11 <hanamesfin67@gmail.com>
2024-11-05 15:57:49 +0300

    test

--- index.js
+++ index.js
@@ -7,11 +7,21 @@ const port = 3000;
 
 
 app.get("/", (req, res) => {
-    const today = new Date();
+    const today = new Date("november 2 ,2024 11:12:01");
     const day = today.getDay();
-    console.log(day)
+    // console.log(day)
+
+    let type = "a weekday";
+    let adv = "Its's a day to work hard ";
+
+    if (day === 0 || day === 6) {
+        type = "weekend";
+        adv = "It's a day to relax and enjoy your weekend ";
+    }
+
     res.render('index.ejs', {
-        dataType:"a weekday",advice:"Its's a day to work hard "});
+        dataType:type,
+        advice:adv,});
     });
 app.listen(port, () => {
     console.log(`server running on port ${port}`);
