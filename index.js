7f2678d6efb91ecf7e1545b6dc2c34ee28ed1d8a
annah11 <hanamesfin67@gmail.com>
2024-11-05 16:22:31 +0300

    test

--- index.js
+++ index.js
@@ -1,28 +1,29 @@
-import express from "express";
-
+import express from 'express';
 
 
 const app = express();
 const port = 3000;
 
-
 app.get("/", (req, res) => {
-    const today = new Date("november 2 ,2024 11:12:01");
+
+    const today = new Date("nov 2 2024 11:23:01");
     const day = today.getDay();
-    // console.log(day)
+    // console.log(day);
+    let type = "weekday";
+    let adv =" a day to work hard";
 
-    let type = "a weekday";
-    let adv = "Its's a day to work hard ";
-
-    if (day === 0 || day === 6) {
+    if (day === 0|| day === 6){
         type = "weekend";
-        adv = "It's a day to relax and enjoy your weekend ";
+        adv = "a day to relax and enjoy the sunshine";
     }
 
-    res.render('index.ejs', {
+    res.render("index.ejs", {
         dataType:type,
-        advice:adv,});
-    });
-app.listen(port, () => {
-    console.log(`server running on port ${port}`);
+        advice:adv,
+        });
+
+});
+
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
 });
